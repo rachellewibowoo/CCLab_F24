2351c7cdb7fb61d5891afdc38c145f4e09617c13
rachellewibowoo <rwo2015@nyu.edu>
2024-10-22 00:10:50 +0800

    Update script.js

--- projectA/script.js
+++ projectA/script.js
@@ -33,7 +33,7 @@ let speedw2 = 1;
 
 function setup() {
   let canvas = createCanvas(800, 800);
-  canvas.parent("p5-canvas-container");
+  //canvas.parent("p5-canvas-container");
   colorMode(HSB);
   let o = 50;
 
@@ -276,7 +276,7 @@ function draw() {
    
   drawFace(x1, y1, z, r1, r2);
   //adding conditionals to make blobby die when it is poked and     expands to a certain point
-  if(z > 380){
+  if(z > 500){
    z =0.00001
   }
   //making the mouth change downwards when blobby expands to a certain point
