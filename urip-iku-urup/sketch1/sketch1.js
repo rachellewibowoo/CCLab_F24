99f67855f507961454f3d47ada0fdd66cd6e07ef
rachellewibowoo <rwo2015@nyu.edu>
2024-12-12 01:05:14 +0800

    -

--- urip-iku-urup/sketch1/sketch1.js
+++ urip-iku-urup/sketch1/sketch1.js
@@ -59,7 +59,7 @@ function mousePressed() {
     scene1 = false;
     scene2 = true;
   }
-  if (scene2 == true && mouseX > 1.5*windowWidth ){
+  if (scene2 == true && mouseX > 1.5*windowWidth && mouseY > windowHeight ){
     scene2 = false; 
     scene3 = true; 
   }
